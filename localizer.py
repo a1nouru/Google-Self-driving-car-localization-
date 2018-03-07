74f3fd8dbae6bd108faba13797184a66261859fc
Nouru Muneza <nizzogentl@gmail.com>
2018-03-06 17:52:57 -0800

    Update localizer.py

--- localizer.py
+++ localizer.py
@@ -2,13 +2,13 @@
 #resulting probability distribution.
 
 p=[0, 1, 0, 0, 0]
-world=['green', 'red', 'red', 'green', 'green']
+world=['green', 'red', 'red', 'green', 'green'] #simple possible positions in the world. 
 measurements = ['red', 'green']
 pHit = 0.6 #probability of location being meet
 pMiss = 0.2 #probability of location missed
 pExact = 0.8 ##probability of exact location 
-pOvershoot = 0.1
-pUndershoot = 0.1
+pOvershoot = 0.1 #Undershooting probability of a certain location. 
+pUndershoot = 0.1 #Overshooting probability of a certain location. 
 
 def sense(p, Z): #sensing the location of the robocar
     q=[]
@@ -20,7 +20,7 @@ def sense(p, Z): #sensing the location of the robocar
         q[i] = q[i] / s
     return q
 
-def move(p, U):
+def move(p, U): #Moving certain steps 
     q = []
     for i in range(len(p)):
         s = pExact * p[(i-U) % len(p)] #calculating the next 1 step position in list
